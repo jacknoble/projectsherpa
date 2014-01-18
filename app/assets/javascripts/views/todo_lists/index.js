f618ef718a9c5efa74fa7c4a54b0f576368f7e11
Jack Noble <jack.noble@gmail.com>
2014-01-17 18:22:41 -0800

    add to new todolist view

--- app/assets/javascripts/views/todo_lists/index.js
+++ app/assets/javascripts/views/todo_lists/index.js
@@ -2,7 +2,7 @@ Sherpa.Views.TodoListIndex = Backbone.View.extend({
 	template: JST["todo_lists/index"],
 	events: {
 		"click button.new_todo_list":"newTodoListForm"
-	}
+	},
 	render: function () {
 		this.$el.html(this.template({lists: this.collection}));
 		return this;
--- app/assets/javascripts/views/todo_lists/new.js
+++ app/assets/javascripts/views/todo_lists/new.js
@@ -1,5 +1,6 @@
 Sherpa.Views.NewTodoList = Backbone.View.extend({
 	template: JST["todo_lists/new"],
+	tagName: "form"
 	render: function() {
 		this.$el.html(this.template());
 		return this;
