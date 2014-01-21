4c63ea315b0d47b67d6093f5d2c78ff8587a40be
Jack Noble <jack.noble@gmail.com>
2014-01-20 17:34:26 -0800

    get basic calendar working

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -3,4 +3,5 @@ class Api::EventsController < ApplicationController
   def index
     @events = current_user.assigned_items
     render 'api/events/index'
+  end
 end
--- app/views/api/events/index.json.jbuilder
+++ app/views/api/events/index.json.jbuilder
@@ -1,2 +1,5 @@
 json.array!(@events) do |event|
-  json.(event, :)
+  json.id event.id
+  json.title event.name
+  json.start event.deadline
+end
